20dba17bf85abec9ada8ed56ba4f4936737f3365
Emilio F <emiliofurrer00@gmail.com>
2025-11-15 13:48:50 +0100

    setup prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,41 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    ...compat.extends(
+        "next/core-web-vitals", // Next.js built-in rules for performance and best practices
+        "next/typescript", // Next.js TypeScript-specific lint rules
+        "plugin:prettier/recommended", // Enables `eslint-config-prettier` + `eslint-plugin-prettier`
+    ),
+    ...compat.config({
+        extends: ["next", "prettier"],
+        rules: {
+            "no-console": "warn", // Warning for console.log
+            quotes: ["error", "double"], // Enforce double quotes
+            "prettier/prettier": "error", // Prettier errors show up in ESLint output
+        },
+    }),
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
 ]);
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  }
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.pexels.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+    /* config options here */
 };
 
 export default nextConfig;

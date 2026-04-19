a8326735c73a64ef63353e6d2ce4a2914192e647
Emilio F <emiliofurrer00@gmail.com>
2026-04-19 09:26:47 +0200

    add navbar to studymode layout

--- app/(studymode)/study/[slug]/layout.tsx
+++ app/(studymode)/study/[slug]/layout.tsx
@@ -1,3 +1,9 @@
+import Navbar from '@/components/landing/Navbar';
+
 export default function StudyModeLayout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-background min-h-screen w-full">{children}</div>;
+  return (
+    <div className="bg-background min-h-screen w-full pt-14">
+      <Navbar /> {children}
+    </div>
+  );
 }

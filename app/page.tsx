7ce1eabbc15987487d151d6216b1370a602a231d
Emilio F <emiliofurrer00@gmail.com>
2026-04-19 09:26:09 +0200

    set isLandingPage prop to true for Navbar on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import StatsSection from '@/components/landing/StatsSection';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-background  font-sans dark:bg-black w-full">
-      <Navbar />
+      <Navbar isLandingPage={true} />
       <main className="flex min-h-screen w-full flex-col items-center  dark:bg-black sm:items-start">
         <HeroSection />
         <StatsSection />

b2c091e493c20a65e5bdc6d378f866ea111c43d8
Emilio F <emiliofurrer00@gmail.com>
2026-04-23 00:49:58 +0200

    remove navbar from study mode layout and add layout files to friends and notification pages

--- app/(studymode)/study/[slug]/layout.tsx
+++ app/(studymode)/study/[slug]/layout.tsx
@@ -1,9 +1,5 @@
-import Navbar from '@/components/landing/Navbar';
+//import Navbar from '@/components/landing/Navbar';
 
 export default function StudyModeLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-background min-h-screen w-full pt-14">
-      <Navbar /> {children}
-    </div>
-  );
+  return <div className="bg-background min-h-screen w-full">{children}</div>;
 }
